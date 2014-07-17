441a09b7a8d60bd9e086536c46b00d4978944a77
Harvey <1448616686@qq.com>
2014-07-17 13:56:51 +0800

    添加注释
    添加注释

--- Hello/Hello/ViewController.h
+++ Hello/Hello/ViewController.h
@@ -11,6 +11,7 @@
 @interface ViewController : UIViewController<UIWebViewDelegate>
 {
     UIWebView *webView;
+    UIActivityIndicatorView *activityIndicatorView;
 }
 
 @end
--- Hello/Hello/ViewController.m
+++ Hello/Hello/ViewController.m
@@ -17,30 +17,36 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    //创建一个UIWebView对象
     webView =[[UIWebView alloc]initWithFrame:CGRectMake(0, 20, 320, 480)];
     webView.scalesPageToFit = YES;
+    //设置代理
     [webView setDelegate:self];
+    //创建NSURLRequest对象
     NSURLRequest *request = [NSURLRequest requestWithURL:[NSURL URLWithString:@"http://www.so.com"]];
+    //添加控件
     [self.view addSubview:webView];
+    //加载
     [webView loadRequest:request];
 }
 
 -(void)webViewDidStartLoad:(UIWebView *)webView
 {
     NSLog(@"开始加载");
-    UIActivityIndicatorView *activity = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleGray];
-    activity.frame =CGRectMake(0, 0, 32.0f, 32.0f);
-    activity.tag=100;
-    activity.center = self.view.center;
-    [self.view addSubview:activity];
-    [activity startAnimating];
+    //创建风火轮对象
+    activityIndicatorView= [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleGray];
+    activityIndicatorView.frame =CGRectMake(0, 0, 32.0f, 32.0f);
+    //设置位置
+    activityIndicatorView.center = self.view.center;
+    [self.view addSubview:activityIndicatorView];
+    //开始动画
+    [activityIndicatorView startAnimating];
 }
 -(void)webViewDidFinishLoad:(UIWebView *)webView
 {
-    //通过全局变量来获取控件
-    UIActivityIndicatorView * activity=(UIActivityIndicatorView *)[self.view viewWithTag:100];
-    [activity stopAnimating];
+    //可以通过全局变量来获取控件
+    //结束动画
+    [activityIndicatorView stopAnimating];
     NSLog(@"加载完成");
 }
 - (void)didReceiveMemoryWarning
